64c04b73022f0a228959f7d440bb344098c81f4c
kimyh <mk.kimyh@gmail.com>
2019-02-15 17:46:21 +0900

    layout reset ongoing . . .

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/default.css">
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <title>비즈키컨설팅</title>
+    </head>
+    <body>
+        <header id="header">
+            <div class="container">
+                <h1>Hello, world!</h1>
+            </div>
+        </header>
+        
+        <main id="content">
+            <div class="container">
+                <h1>Hello, world!</h1>
+            </div>
+        </main>
+        
+        <footer id="footer">
+            <article class="partner">
+                <div class="container">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="">
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="">
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+            <div class="container">
+                <h1>Hello, world!</h1>
+            </div>
+        </footer>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/assets/js/prefixfree.min.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    </body>
+</html>
